bc33bd0774719a26e2d0145ddfca035defd15ab9
Garcialu29 <garcialu422@gmail.com>
2024-08-13 11:36:14 -0600

    update

--- Views/Template/header_admin.php
+++ Views/Template/header_admin.php
@@ -23,10 +23,12 @@
     <!-- Navbar-->
     <header class="app-header"><a class="app-header__logo" href="<?= base_url(); ?>/inicio">Traex</a>
       <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
+      
+      
       <!-- Navbar Right Menu-->
-      <ul class="app-nav">
-        <!--Notification Menu COMENTADO TODO DE NOTIFICACION-->
-       <!-- <li class="dropdown"><a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Show notifications"><i class="fa fa-bell-o fa-lg"></i></a>
+      <!--<ul class="app-nav">COMENTADO TODO DE NOTIFICACION
+        Notification Menu 
+       <li class="dropdown"><a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Show notifications"><i class="fa fa-bell-o fa-lg"></i></a>
           <ul class="app-notification dropdown-menu dropdown-menu-right">
             <li class="app-notification__title">Tienes 2 notificaciones </li>
             <div class="app-notification__content">
@@ -39,9 +41,9 @@
                   <div>
                     <p class="app-notification__message">Mail server not working</p>
                     <p class="app-notification__meta">5 min ago</p>
-                  </div></a></li>-->
+                  </div></a></li>
      
-              </div>
+              </div> -->
            <!-- </div>
             <li class="app-notification__footer"><a href="#">See all notifications.</a></li>
           </ul>
