2bc31eb885eedfabaec7eb7e9e205a0aa5236d2b
Garcialu29 <garcialu422@gmail.com>
2024-08-13 00:49:53 -0600

    ups

--- Views/Reiniciar_contraseña/reiniciar_contraseña.php
+++ Views/Reiniciar_contraseña/reiniciar_contraseña.php
@@ -40,7 +40,7 @@
                      
                 </label><br>
                 
-                    <input  id="op_preguntas" type="" name="opcion" > 
+                    <input  id="op_preguntas"  > 
                 </label>
               
             </fieldset>
